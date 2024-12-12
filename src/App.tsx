d3cf1625c5ce685064eba9b6a86c3b258580e19c
Glen Marti <martidicianolold@gmail.com>
2024-12-12 09:34:00 +0100

    feat(layout): build responive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Button } from "./components/ui/button";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      <GridItem area="aside" bg="gold" hideBelow="lg" hide>
+        Aside
+      </GridItem>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 

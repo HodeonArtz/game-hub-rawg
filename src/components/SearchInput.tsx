8380637aa5008f1acfed93fda39a772a7da658ec
Glen Marti <martidicianolold@gmail.com>
2024-12-16 09:10:55 +0100

    feat(search): implement searching

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,29 @@
 import { Input } from "@chakra-ui/react";
 import { InputGroup } from "./ui/input-group";
 import { BsSearch } from "react-icons/bs";
+import { useRef } from "react";
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const SearchInput = ({ onSearch }: Props) => {
+  const searchRef = useRef<HTMLInputElement>(null);
 
-const SearchInput = () => {
   return (
-    <InputGroup flex="1" startElement={<BsSearch />}>
-      <Input borderRadius={20} placeholder="Search games.." variant="subtle" />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (searchRef.current) onSearch(searchRef.current.value);
+      }}
+    >
+      <InputGroup flex="1" startElement={<BsSearch />}>
+        <Input
+          ref={searchRef}
+          borderRadius={20}
+          placeholder="Search games.."
+          variant="subtle"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
